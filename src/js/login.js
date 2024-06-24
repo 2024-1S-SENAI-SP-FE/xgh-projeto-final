2df87ee4e81c0a24262aeb0d084ae62241222809
evypersonal <evellymaria2015@outlook.com>
2024-06-24 15:49:00 -0300

    chore: removendo comentários de teste no arquivo login.js

--- src/js/login.js
+++ src/js/login.js
@@ -1,30 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     const botoesPreco = document.querySelectorAll('.opcoes-preco button');
-//     const opcoesTracao = document.querySelectorAll('.opcoes-tracao .opcao');
-//     const opcoesPintura = document.querySelectorAll('.opcoes-pintura .pintura');
-
-//     botoesPreco.forEach(botao => {
-//         botao.addEventListener('click', function () {
-//             botoesPreco.forEach(btn => btn.classList.remove('ativo'));
-//             this.classList.add('ativo');
-//         });
-//     });
-
-//     opcoesTracao.forEach(opcao => {
-//         opcao.addEventListener('click', function () {
-//             opcoesTracao.forEach(opt => opt.classList.remove('ativo'));
-//             this.classList.add('ativo');
-//         });
-//     });
-
-//     opcoesPintura.forEach(pintura => {
-//         pintura.addEventListener('click', function () {
-//             opcoesPintura.forEach(pnt => pnt.classList.remove('ativo'));
-//             this.classList.add('ativo');
-//         });
-//     });
-// });
-
 
 const usuarios = [
     {
