9e10a556df708e232ff9429fcfb42ffa9705f193
evypersonal <evellymaria2015@outlook.com>
2024-06-24 17:43:45 -0300

    feat: implementando metodo GET na tela de login

--- src/js/login.js
+++ src/js/login.js
@@ -1,23 +1,3 @@
-// JSON de usuários
-const usuarios = [
-    {
-        "id": "test@test.com", 
-        "nome": "",
-        "senha": "senha1"
-    },
-    {
-        "id": "", 
-        "nome": "",
-        "senha": ""
-    },
-    {
-        "id": "", 
-        "nome": "",
-        "senha": ""
-    }
-];
-
-// Validação de Login
 document.querySelector('#btnLogin').addEventListener('click', ()=>{
     validLogin();
 })
@@ -27,25 +7,33 @@ function validLogin(){
     const password = document.querySelector('#password').value;
     let loginSucess = false;
 
-    
+    const requisicaoGet = new Request('http://localhost:3000/usuarios', {
+            "method": "GET",
+            "headers": {
+                "Content-type": "application/json"
+            }
+        });
 
-    for(let i = 0; i < usuarios.length; i++){
-        if(usuarios[i].id === email && usuarios[i].senha === password){
-            loginSucess = true;
-            break;
+    fetch(requisicaoGet)
+    .then(resposta => resposta.json())
+    .then(resposta => {
+        for(let i = 0; i < resposta.length; i++){
+            if(resposta[i].email === email && resposta[i].senha === password){
+                loginSucess = true;
+                break;
+            }
         }
-    }
-
-
-    if(loginSucess){
-        // resultElement.innerHTML= "Login bem sucedido"
-        window.alert('Login bem-sucedido');
-        window.location.href = "http://127.0.0.1:5501/index.html"
-        // ====================linha a verificar
-        document.querySelector('#divLogado').style.display = "block"
-    }else{
-        window.alert('Email ou Senha inválido')
-    }
+        if(loginSucess){
+            // resultElement.innerHTML= "Login bem sucedido"
+            window.alert('Login bem-sucedido');
+            window.location.href = "http://127.0.0.1:5501/index.html"
+            // ====================linha a verificar
+            document.querySelector('#divLogado').style.display = "block"
+        }else{
+            window.alert('Email ou Senha inválido')
+        }
+    })
+    .catch(erro => console.log(erro));
 }
 
 // Botão de visualização de senha
