c7f0e9aa15e6e467421e1e64e9bebbcf83bbd266
guilhermeaalves <contatoguilhermealv@gmail.com>
2024-06-25 09:05:04 -0300

    fix: corrigindo função de ver a senha

--- src/js/login.js
+++ src/js/login.js
@@ -28,52 +28,52 @@
 
 const usuarios = [
     {
-        "id": "test@test.com", 
+        "id": "test@test.com",
         "senha": "senha1"
     },
     {
-        "id": "user2", 
+        "id": "user2",
         "senha": "senha2"
     },
     {
-        "id": "user3", 
+        "id": "user3",
         "senha": "senha3"
     }
 ];
 
-document.querySelector('#btnLogin').addEventListener('click', ()=>{
+document.querySelector('#btnLogin').addEventListener('click', () => {
     validLogin();
 })
 
-function validLogin(){
+function validLogin() {
     const email = document.querySelector('#email').value;
     const password = document.querySelector('#password').value;
     let loginSucess = false;
 
-    for(let i = 0; i < usuarios.length; i++){
-        if(usuarios[i].id === email && usuarios[i].senha === password){
+    for (let i = 0; i < usuarios.length; i++) {
+        if (usuarios[i].id === email && usuarios[i].senha === password) {
             loginSucess = true;
             break;
         }
     }
 
 
-    if(loginSucess){
+    if (loginSucess) {
         // resultElement.innerHTML= "Login bem sucedido"
         window.alert('Login bem-sucedido');
         window.location.href = "http://127.0.0.1:5501/index.html"
         // ====================linha a verificar
         document.querySelector('#divLogado').style.display = "block"
-    }else{
+    } else {
         window.alert('Email ou Senha inválido')
     }
 }
 
 document.querySelector('#btSenha').addEventListener('click', () => {
     const passwordInput = document.querySelector('#password');
-    if(passwordInput.type === 'password'){
+    if (passwordInput.type === 'text') {
+        passwordInput.type = 'password';
+    } else {
         passwordInput.type = 'text';
-        }else{
-            passwordInput.type = 'password';
-            }
+    }
 })
