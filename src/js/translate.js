a07b85c5e7a3ee4a34141057d674cf4457974019
guilhermeaalves <contatoguilhermealv@gmail.com>
2024-06-26 11:22:08 -0300

    fix: arrumando tradução

--- src/js/translate.js
+++ src/js/translate.js
@@ -1,9 +1,22 @@
-function googleTranslateElementInit () {
+document.addEventListener('DOMContentLoaded', function() {
+  const btnTranslate = document.querySelector('#troca-idioma');
+  const selectLang = document.querySelector('#google_translate_element');
+
+    function oculta() {
+      if (selectLang.style.display === 'none') {
+        document.querySelector('#google_translate_element').style.display = 'block';
+      } else {
+        document.querySelector('#google_translate_element').style.display = 'none';
+      }
+    }
+
+    btnTranslate.addEventListener('click', oculta);
+    oculta(); // Chama a função oculta no carregamento da página, se necessário
+});
+
+function googleTranslateElementInit() {
   new google.translate.TranslateElement(
-    {
-      pageLanguage: 'pt-BR', // Idioma padrão do site
-      layout: google.translate.TranslateElement.InlineLayout.SIMPLE
-    },
+    { pageLanguage: 'pt-BR' }, // Idioma padrão do site
     'google_translate_element'
   )
 }
