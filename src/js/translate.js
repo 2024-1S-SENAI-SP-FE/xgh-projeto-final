7b48331fc005a11372083171bb90a33bfd0a7d61
guilhermeaalves <contatoguilhermealv@gmail.com>
2024-06-27 09:27:21 -0300

    fix: corrigindo api de tradução

--- src/js/translate.js
+++ src/js/translate.js
@@ -1,22 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const btnTranslate = document.querySelector('#troca-idioma');
-  const selectLang = document.querySelector('#google_translate_element');
+const btnTranslate = document.querySelector('#troca-idioma');
+const selectLang = document.querySelector('#google_translate_element');
 
-    function oculta() {
-      if (selectLang.style.display === 'none') {
+btnTranslate.addEventListener('click', function oculta() {
+    if (selectLang.style.display === 'none') {
         document.querySelector('#google_translate_element').style.display = 'block';
-      } else {
+    } else {
         document.querySelector('#google_translate_element').style.display = 'none';
-      }
     }
-
-    btnTranslate.addEventListener('click', oculta);
-    oculta(); // Chama a função oculta no carregamento da página, se necessário
 });
 
+document.addEventListener('DOMContentLoaded', oculta())
+
 function googleTranslateElementInit() {
-  new google.translate.TranslateElement(
-    { pageLanguage: 'pt-BR' }, // Idioma padrão do site
-    'google_translate_element'
-  )
+    new google.translate.TranslateElement(
+        { pageLanguage: 'pt-BR' }, // Idioma padrão do site
+        'google_translate_element'
+    )
 }
