6c8b73439bf8687ba983e34862ae471b38756556
joaopaulofserafim <jpfserafim72gmail.com>
2024-06-27 20:36:07 -0300

    chore: criando funcao para a section

--- src/js/compra.js
+++ src/js/compra.js
@@ -10,26 +10,26 @@ elements.forEach(element => {
 function pegaCor() {
   let activeElement = document.querySelector('.conteudo-cores > div.active > div > span');
   if (activeElement) {
-      return activeElement.innerHTML;
+    return activeElement.innerHTML;
   }
   return null;
 }
 
 // Adiciona evento de clique aos divs de cores
 document.querySelectorAll('.conteudo-cores > div').forEach(div => {
-  div.addEventListener('click', function() {
-      document.querySelectorAll('.conteudo-cores > div').forEach(d => {
-          d.classList.remove('active');
-      });
-      this.classList.add('active');
-      let activeElement = pegaCor();
-      console.log(`${activeElement}`);
+  div.addEventListener('click', function () {
+    document.querySelectorAll('.conteudo-cores > div').forEach(d => {
+      d.classList.remove('active');
+    });
+    this.classList.add('active');
+    let activeElement = pegaCor();
+    console.log(`${activeElement}`);
   });
 });
 
 // Revisão de compra
 document.querySelector('#btnCompra').addEventListener('click', () => {
-  
+
   const cor = pegaCor();
   const frete = document.querySelector('#frete').innerHTML;
   console.log(`${cor}`);
@@ -38,7 +38,7 @@ document.querySelector('#btnCompra').addEventListener('click', () => {
 
 });
 
-  // Div forma de pagamento OFICIAL 
+// Div forma de pagamento OFICIAL 
 // document.querySelector('#btnCompra').addEventListener('click', ()=>{
 //   const frete = document.querySelector('#frete').innerHTML;
 //   if(frete != '')
@@ -50,14 +50,14 @@ document.querySelector('#btnCompra').addEventListener('click', () => {
 // })
 
 // Div forma de pagamento TEMPORARIA
-document.querySelector('#btnCompra').addEventListener('click', ()=>{
-    document.querySelector('#forma-pagamento').style.display = 'block';
+document.querySelector('#btnCompra').addEventListener('click', () => {
+  document.querySelector('#forma-pagamento').style.display = 'block';
 });
 
 // Seleção pagamento
 
 // Boleto
-document.querySelector('#btnBoleto').addEventListener('click', ()=>{
+document.querySelector('#btnBoleto').addEventListener('click', () => {
   document.querySelector('#pag-boleto').style.display = 'block'
   document.querySelector('#pag-cartao').style.display = 'none'
   document.querySelector('#btnRevizarBoleto').style.display = 'block'
@@ -65,17 +65,17 @@ document.querySelector('#btnBoleto').addEventListener('click', ()=>{
 })
 
 // Cartão
-document.querySelector('#btnCartao').addEventListener('click', ()=>{
+document.querySelector('#btnCartao').addEventListener('click', () => {
   document.querySelector('#pag-boleto').style.display = 'none'
   document.querySelector('#pag-cartao').style.display = 'block'
   document.querySelector('#btnRevizarCartao').style.display = 'block'
   document.querySelector('#nome-cartao').focus();
-  
+
 })
 
 
 // Botão de Revizar Compra
-document.querySelector('#btnRevizarBoleto').addEventListener('click',()=>{
+document.querySelector('#btnRevizarBoleto').addEventListener('click', () => {
   const pagBoleto = document.querySelector('#pag-boleto').style.display;
 
   const nomeBoleto = document.querySelector('#nome').value;
@@ -90,39 +90,55 @@ document.querySelector('#btnRevizarBoleto').addEventListener('click',()=>{
   } else {
     window.alert('Preencha todos os campos para finalizar a compra!');
     document.querySelector('#nome').focus();
-    }
-  });
+  }
+});
 
-  // Botão de Revizar Cartao
-  document.querySelector('#btnRevizarCartao').addEventListener('click', ()=>{
-    const pagCartao = document.querySelector('#pag-cartao').style.display;
+// Botão de Revizar Cartao
+document.querySelector('#btnRevizarCartao').addEventListener('click', () => {
+  const pagCartao = document.querySelector('#pag-cartao').style.display;
 
-    const nomeCartao = document.querySelector('#nome-cartao').value;
-    const numeroCartao = document.querySelector('#numero-cartao').value;
-    const mesCartao = document.querySelector('#mes').value;
-    const anoCartao = document.querySelector('#ano').value;
-    const cvvCartao = document.querySelector('#cvv').value;
-    
-    // verificar correção de erro
-    if((nomeCartao !== '') && (numeroCartao !== '') && (mesCartao !== '') && (anoCartao !== '') && (cvvCartao !== '')){
-      // window.alert('Compra realizada com sucesso!');
-      document.querySelector('#resumo-compra').style.display = 'block';
-    } else {
-      window.alert('Preencha todos os campos para finalizar a compra!');
-      document.querySelector('#nome-cartao').focus();
-    }
-  });
+  const nomeCartao = document.querySelector('#nome-cartao').value;
+  const numeroCartao = document.querySelector('#numero-cartao').value;
+  const mesCartao = document.querySelector('#mes').value;
+  const anoCartao = document.querySelector('#ano').value;
+  const cvvCartao = document.querySelector('#cvv').value;
 
-  // Finalizar
+  // verificar correção de erro
+  if ((nomeCartao !== '') && (numeroCartao !== '') && (mesCartao !== '') && (anoCartao !== '') && (cvvCartao !== '')) {
+    // window.alert('Compra realizada com sucesso!');
+    document.querySelector('#resumo-compra').style.display = 'block';
+  } else {
+    window.alert('Preencha todos os campos para finalizar a compra!');
+    document.querySelector('#nome-cartao').focus();
+  }
+});
 
-  document.querySelector('#btnFinalizar').addEventListener('click', ()=>{
-    document.querySelector('#finaliza-compra').style.display = 'flex';
-  })
+// Finalizar
 
-  // bt close finalizar 
+document.querySelector('#btnFinalizar').addEventListener('click', () => {
+  document.querySelector('#finaliza-compra').style.display = 'flex';
+})
 
-  // document.querySelector('#bt-close').addEventListener('click', ()=>{
-  //   const suporte = document.querySelector('#suporte');
-  //   suporte.style.display = 'none';
+document.querySelector('#btnFinalizar').addEventListener('click', function () {
+  var conteudo = document.querySelector('.container-finalizar');
+  conteudo.style.display = 'flex';
 
+  var classAtual = document.querySelector('.container-finalizar');
+  setTimeout(function () {
+    classAtual.classList.toggle('hover-finaliza');
+  }, 600);
+
+  setTimeout(function() {
+    var container = document.querySelector('#finaliza-compra')
+      container.style.display = 'none';
+  }, 8000); 
+});
+
+// Muda cor svg 
+document.querySelector('#btnFinalizar').addEventListener('click', function () {
+  var classAtual = document.querySelector('#Capa_1');
+  setTimeout(function () {
+    classAtual.classList.toggle('hover-svg');
+  }, 600);
+});
 
